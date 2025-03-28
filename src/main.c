f30d6655dd69f567109471f4bdb8320e8aa50295
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-03-28 15:58:19 -0500

    init main

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include "pico/stdlib.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+void main() {
+    stdio_init_all();
+
+    while (1) {
+        // Infinite loop to prevent returning from main
+    }
+}
