2f484ff679fe4ca01e691786a3cde190440d9b69
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-04-02 03:42:02 -0500

    add unimplemented send functions

--- src/st7796/st7796.c
+++ src/st7796/st7796.c
@@ -14,3 +14,17 @@ void st7796_init(lv_display_t *disp, spi_inst_t *spi, int dma_channel)
         st7796.spi = spi;
         st7796.dma_channel = dma_channel;
 }
+
+/* Send short command to the LCD. This function shall wait until the transaction finishes. */
+static int32_t st7796_send_cmd(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, const uint8_t *param, size_t param_size)
+{
+        // UNIMPLEMENTED
+        return 0;
+}
+
+/* Send large array of pixel data to the LCD. If necessary, this function has to do the byte-swapping. This function can do the transfer in the background. */
+static int32_t st7796_send_color(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, uint8_t *param, size_t param_size)
+{
+        // UNIMPLEMENTED
+        return 0;
+}
