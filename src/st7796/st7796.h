573d7b469f6b0a4c8d64ecc61562f5bc410ccd02
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-04-02 03:40:45 -0500

    add st7796.h ifndef

--- src/st7796/st7796.h
+++ src/st7796/st7796.h
@@ -1,5 +1,10 @@
+#ifndef LV_ST7796_H
+#define LV_ST7796_H
+
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
 #include "lvgl.h"
 
 void st7796_init(lv_display_t *disp, spi_inst_t *spi, int dma_channel);
+
+#endif /*ST7796_H*/
