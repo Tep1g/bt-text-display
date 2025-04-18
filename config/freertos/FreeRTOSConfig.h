41f334d641a3be7c9a10787d7417e09c43bcc673
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-04-18 15:57:49 -0500

    update freertos config to support smp

--- config/freertos/FreeRTOSConfig.h
+++ config/freertos/FreeRTOSConfig.h
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS V202111.00
+ * FreeRTOS V202212.00
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -19,10 +19,9 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * http://www.FreeRTOS.org
- * http://aws.amazon.com/freertos
+ * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
  *
- * 1 tab == 4 spaces!
  */
 
  #ifndef FREERTOS_CONFIG_H
@@ -44,7 +43,7 @@
  #define configUSE_PREEMPTION                    1
  #define configUSE_TICKLESS_IDLE                 0
  #define configUSE_IDLE_HOOK                     0
- #define configUSE_TICK_HOOK                     0
+ #define configUSE_TICK_HOOK                     1
  #define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 )
  #define configMAX_PRIORITIES                    32
  #define configMINIMAL_STACK_SIZE                ( configSTACK_DEPTH_TYPE ) 256
@@ -61,8 +60,7 @@
  #define configUSE_QUEUE_SETS                    1
  #define configUSE_TIME_SLICING                  1
  #define configUSE_NEWLIB_REENTRANT              0
- // todo need this for lwip FreeRTOS sys_arch to compile
- #define configENABLE_BACKWARD_COMPATIBILITY     1
+ #define configENABLE_BACKWARD_COMPATIBILITY     0
  #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
  
  /* System */
@@ -76,18 +74,14 @@
  #define configAPPLICATION_ALLOCATED_HEAP        0
  
  /* Hook function related definitions. */
- #define configCHECK_FOR_STACK_OVERFLOW          0
- #define configUSE_MALLOC_FAILED_HOOK            0
+ #define configCHECK_FOR_STACK_OVERFLOW          2
+ #define configUSE_MALLOC_FAILED_HOOK            1
  #define configUSE_DAEMON_TASK_STARTUP_HOOK      0
  
  /* Run time and task stats gathering related definitions. */
  #define configGENERATE_RUN_TIME_STATS           0
  #define configUSE_TRACE_FACILITY                1
  #define configUSE_STATS_FORMATTING_FUNCTIONS    0
- 
- /* Co-routine related definitions. */
- #define configUSE_CO_ROUTINES                   0
- #define configMAX_CO_ROUTINE_PRIORITIES         1
  
  /* Software timer related definitions. */
  #define configUSE_TIMERS                        1
@@ -102,13 +96,11 @@
  #define configMAX_API_CALL_INTERRUPT_PRIORITY   [dependent on processor and application]
  */
  
- #if FREE_RTOS_KERNEL_SMP // set by the RP2040 SMP port of FreeRTOS
  /* SMP port only */
- #define configNUM_CORES                         2
+ #define configNUMBER_OF_CORES                   2
  #define configTICK_CORE                         0
- #define configRUN_MULTIPLE_PRIORITIES           1
+ #define configRUN_MULTIPLE_PRIORITIES           0
  #define configUSE_CORE_AFFINITY                 1
- #endif
  
  /* RP2040 specific */
  #define configSUPPORT_PICO_SYNC_INTEROP         1
@@ -138,5 +130,9 @@
  #define INCLUDE_xQueueGetMutexHolder            1
  
  /* A header file that defines trace macro can be included here. */
+ 
+ /* SMP Related config. */
+ #define configUSE_PASSIVE_IDLE_HOOK             0
+ #define portSUPPORT_SMP                         1
  
  #endif /* FREERTOS_CONFIG_H */
