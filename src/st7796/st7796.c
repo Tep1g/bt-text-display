e95f64f2650175d0e7a2432e59c0f901b783d13a
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-04-20 21:09:05 -0500

    define st7796_init() after declaration of send functions
    The static send functions are referenced by st7796_init() so they need to be declared before it

--- src/st7796/st7796.c
+++ src/st7796/st7796.c
@@ -20,47 +20,6 @@ static struct ST7796 {
         bool bus_busy;
 } st7796;
 
-void st7796_init(
-        uint32_t st7796_hor_res, 
-        uint32_t st7796_ver_res, 
-        lv_lcd_flag_t st7796_flag, 
-        spi_inst_t *spi, 
-        int cs_gpio, 
-        int dcx_gpio, 
-        int rst_gpio, 
-        int dma_channel, 
-        uint dma_irq_index, 
-        dma_channel_config *dma_config
-) {
-        if (st7796.inited) {
-                return;
-        }
-        st7796.disp = lv_st7796_create(st7796_hor_res, st7796_ver_res, st7796_flag, st7796_send_cmd, st7796_send_color);;
-        st7796.spi = spi;
-        st7796.cs_gpio = cs_gpio;
-        st7796.dcx_gpio = dcx_gpio;
-        st7796.rst_gpio = rst_gpio;
-        st7796.dma_channel = dma_channel;
-        st7796.dma_irq_index = dma_irq_index;
-        st7796.dma_config = dma_config;
-        st7796.bus_busy = false;
-
-        // Reset the LCD
-        gpio_put(st7796.cs_gpio, 0);
-        gpio_put(st7796.dcx_gpio, 1);
-
-        gpio_put(st7796.rst_gpio, 1);
-        sleep_ms(50);
-        gpio_put(st7796.rst_gpio, 0);
-        sleep_ms(100);
-        gpio_put(st7796.rst_gpio, 1);
-        sleep_ms(100);
-
-        gpio_put(st7796.cs_gpio, 1);
-
-        st7796.inited = true;
-}
-
 /* Send short command to the LCD. This function shall wait until the transaction finishes. */
 static void st7796_send_cmd(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, const uint8_t *param, size_t param_size)
 {
@@ -115,4 +74,45 @@ void st7796_send_color_callback()
                 st7796.bus_busy = false;
                 lv_display_flush_ready(st7796.disp);
         }
+}
+
+void st7796_init(
+        uint32_t st7796_hor_res, 
+        uint32_t st7796_ver_res, 
+        lv_lcd_flag_t st7796_flag, 
+        spi_inst_t *spi, 
+        int cs_gpio, 
+        int dcx_gpio, 
+        int rst_gpio, 
+        int dma_channel, 
+        uint dma_irq_index, 
+        dma_channel_config *dma_config
+) {
+        if (st7796.inited) {
+                return;
+        }
+        st7796.disp = lv_st7796_create(st7796_hor_res, st7796_ver_res, st7796_flag, st7796_send_cmd, st7796_send_color);;
+        st7796.spi = spi;
+        st7796.cs_gpio = cs_gpio;
+        st7796.dcx_gpio = dcx_gpio;
+        st7796.rst_gpio = rst_gpio;
+        st7796.dma_channel = dma_channel;
+        st7796.dma_irq_index = dma_irq_index;
+        st7796.dma_config = dma_config;
+        st7796.bus_busy = false;
+
+        // Reset the LCD
+        gpio_put(st7796.cs_gpio, 0);
+        gpio_put(st7796.dcx_gpio, 1);
+
+        gpio_put(st7796.rst_gpio, 1);
+        sleep_ms(50);
+        gpio_put(st7796.rst_gpio, 0);
+        sleep_ms(100);
+        gpio_put(st7796.rst_gpio, 1);
+        sleep_ms(100);
+
+        gpio_put(st7796.cs_gpio, 1);
+
+        st7796.inited = true;
 }
