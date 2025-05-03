3157f4be70540fa4bacaff9c209cb855f99f5c46
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-05-03 18:20:17 -0500

    put lcd task on core 0

--- src/main.c
+++ src/main.c
@@ -19,7 +19,10 @@ void vApplicationTickHook( void ) {}
 void main() {
     stdio_init_all();
 
-    xTaskCreate(lcd_task, "LCD Task", 2048, NULL, 1, NULL);
+    TaskHandle_t lcd_task_handle;
+    xTaskCreate(lcd_task, "LCD Task", 2048, NULL, 1, &(lcd_task_handle));
+    UBaseType_t lcd_task_uxCoreAffinityMask = 1 << 0; // Set core affinity to core 0
+    vTaskCoreAffinitySet(lcd_task_handle, lcd_task_uxCoreAffinityMask);
     vTaskStartScheduler();
 
     while (1) {
