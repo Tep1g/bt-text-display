9339fdb35188729e486d234d43856d8ff2b3665e
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-05-22 20:03:52 -0500

    remove infinite while loop from top level main() and return 0

--- src/main.c
+++ src/main.c
@@ -20,12 +20,10 @@ void main() {
     stdio_init_all();
 
     TaskHandle_t lcd_task_handle;
-    xTaskCreate(lcd_task, "LCD Task", 2048, NULL, 1, &(lcd_task_handle));
+    xTaskCreate(lcd_task, "LCD Task", 2048, NULL, 1, &lcd_task_handle);
     UBaseType_t lcd_task_uxCoreAffinityMask = 1 << 0; // Set core affinity to core 0
     vTaskCoreAffinitySet(lcd_task_handle, lcd_task_uxCoreAffinityMask);
     vTaskStartScheduler();
 
-    while (1) {
-        // Infinite loop to prevent returning from main
-    }
+    return 0;
 }
