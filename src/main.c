77d35cd94d732f94a0578852fa62570d6cadb112
Tep1g <66882646+Tep1g@users.noreply.github.com>
2025-05-26 21:18:07 -0500

    add lcd task stack size macro

--- src/main.c
+++ src/main.c
@@ -3,11 +3,13 @@
 #include "task.h"
 #include "lcd.h"
 
+#define LCD_TASK_STACK_SIZE 2048
+
 int main() {
     stdio_init_all();
 
     TaskHandle_t lcd_task_handle;
-    xTaskCreate(lcd_task, "LCD Task", 2048, NULL, 1, &lcd_task_handle);
+    xTaskCreate(lcd_task, "LCD Task", LCD_TASK_STACK_SIZE, NULL, 1, &lcd_task_handle);
     UBaseType_t lcd_task_uxCoreAffinityMask = 1 << 0; // Set core affinity to core 0
     vTaskCoreAffinitySet(lcd_task_handle, lcd_task_uxCoreAffinityMask);
     vTaskStartScheduler();
